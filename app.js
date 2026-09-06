a5e73876da7bbb239d279adcea394047297cb60c
wiem ben nasser <wiembennasser3737@gmail.com>
2026-09-06 15:03:15 +0100

    Update parking availability

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.send(`
     <h1>🅿️ Smart Parking</h1>
     <h2>Welcome to Smart Parking DevOps Project 🚀</h2>
-    <p>Available places: 2 / 4</p>
+    <p>Available places: 3 / 4</p>
   `);
 });
 
