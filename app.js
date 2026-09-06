a658d018f764dc9261ba68a47d952081381246b1
wiem ben nasser <wiembennasser3737@gmail.com>
2026-09-06 10:59:25 +0100

    Initial Smart Parking application

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🅿️ Smart Parking</h1>
+    <h2>Welcome to Smart Parking DevOps Project 🚀</h2>
+    <p>Available places: 2 / 4</p>
+  `);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Application running on port ${PORT}`);
+});
